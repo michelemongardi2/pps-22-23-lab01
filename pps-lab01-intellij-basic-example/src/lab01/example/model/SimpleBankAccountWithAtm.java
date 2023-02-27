532dbaeb2ed657e972801a34805abec80c0aca35
michele.mongardi2 <michele.mongardi2@studio.unibo.it>
2023-02-27 11:26:35 +0100

    Refactoring

--- pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -1,42 +1,15 @@
 package lab01.example.model;
 
-public class SimpleBankAccountWithAtm implements BankAccount {
+public class SimpleBankAccountWithAtm extends SimpleBankAccount implements BankAccount {
 
-    private final AccountHolder accountHolder;
-    private double balance;
-    public SimpleBankAccountWithAtm(AccountHolder accountHolder, double balance) {
-        this.accountHolder = accountHolder;
-        this.balance = balance;
-    }
-
-    @Override
-    public AccountHolder getHolder() {
-        return this.accountHolder;
-    }
-
-    @Override
-    public double getBalance() {
-        return this.balance;
-    }
-
-    @Override
-    public void deposit(final int userID, final double depositAmount) {
-        if (checkUser(userID)) {
-            this.balance += depositAmount;
-        }
+    public SimpleBankAccountWithAtm(AccountHolder holder, double balance) {
+        super(holder, balance);
     }
 
     @Override
     public void depositWithAtm(int userID, int depositAmountWithATM) {
         if(checkUser(userID) && isAmountGreaterThanOne(depositAmountWithATM)) {
             this.balance =+ depositAmountWithATM-1;
-        }
-    }
-
-    @Override
-    public void withdraw(int userID, double withdrawAmout) {
-        if (checkUser(userID) && isWithdrawAllowed(withdrawAmout)) {
-            this.balance -= withdrawAmout;
         }
     }
 
@@ -56,6 +29,6 @@ public class SimpleBankAccountWithAtm implements BankAccount {
     }
 
     private boolean checkUser(final int id) {
-        return this.accountHolder.getId() == id;
+        return this.holder.getId() == id;
     }
 }
