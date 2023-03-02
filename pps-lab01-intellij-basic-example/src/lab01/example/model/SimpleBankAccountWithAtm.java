3690f7926995bd213ea1965663bf10cdb4642438
michele.mongardi2 <michele.mongardi2@studio.unibo.it>
2023-03-02 21:32:52 +0100

    Added description ex1

--- pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -1,5 +1,16 @@
 package lab01.example.model;
 
+/**
+ * Exercise 1 - IntelliJ Basics, Software Quality and Tests
+ * Implement a new version of a bank account, allowing the deposit and
+ * the withdrawal also using the ATM. Each transaction done with the
+ * ATM implies paying a 1$ fee.
+ *   - The new bank account must implement the BankAccount interface and
+ *     coded into a new class SimpleBankAccountWithAtm
+ *   - It is requested to provide a new test class for the new bank account
+ *     (SimpleBankAccountWithAtmTest)
+ */
+
 public class SimpleBankAccountWithAtm extends SimpleBankAccount implements BankAccountWithAtm {
 
     public static final int FEE = 1;
