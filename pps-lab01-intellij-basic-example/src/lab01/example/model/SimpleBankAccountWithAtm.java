5bacb5c37a5b0f3e31c29fc07bb24b32787a1772
michele.mongardi2 <michele.mongardi2@studio.unibo.it>
2023-03-02 21:21:52 +0100

    Refactor SimpleBankAccountWithAtm

--- pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -2,6 +2,8 @@ package lab01.example.model;
 
 public class SimpleBankAccountWithAtm extends SimpleBankAccount implements BankAccountWithAtm {
 
+    public static final int FEE = 1;
+
     public SimpleBankAccountWithAtm(AccountHolder holder, double balance) {
         super(holder, balance);
     }
@@ -9,26 +11,18 @@ public class SimpleBankAccountWithAtm extends SimpleBankAccount implements BankA
     @Override
     public void depositWithAtm(int userID, int depositAmountWithATM) {
         if(checkUser(userID) && isAmountGreaterThanOne(depositAmountWithATM)) {
-            this.balance =+ depositAmountWithATM-1;
+            this.balance =+ depositAmountWithATM-FEE;
         } else throw new IllegalStateException("User not found or illegal deposit amount");
     }
 
     @Override
     public void withdrawWithAtm(int userID, int withdrawAmoutWithATM) {
-        if (checkUser(userID) && isWithdrawAllowed(withdrawAmoutWithATM)) {
-            this.balance -= withdrawAmoutWithATM+1;
+        if (checkUser(userID) && isWithdrawAllowed(withdrawAmoutWithATM+FEE)) {
+            this.balance -= withdrawAmoutWithATM+FEE;
         } else throw new IllegalStateException("User not found or illegal withdraw amount");
     }
 
     private boolean isAmountGreaterThanOne(int depositAmountWithATM) {
         return depositAmountWithATM > 1;
-    }
-
-    private boolean isWithdrawAllowed(final double amount) {
-        return this.balance >= amount+1;
-    }
-
-    private boolean checkUser(final int id) {
-        return this.holder.getId() == id;
     }
 }
